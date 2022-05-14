d9a47d04f12ca62c9ef04f611a25fd4d9832c6ee
Camila Gallegos <camigm1@gmail.com>
2022-05-13 23:04:18 -0600

    update

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  for (let i = 0; i < num; i++) {
+    return string;
+  }
 };
 
+repeatString("hey", 3);
 // Do not edit below this line
 module.exports = repeatString;
